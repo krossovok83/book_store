9cbbf5d547f17699a79a159c2ac7608143c66537
krossovok83 <krossovok@ukr.net>
2022-01-30 14:54:11 +0200

    Some Fix omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, '1378685569201332', '7ac70d44467966e8e351d8b07a603ea3', scope: 'user,public_repo'
+  config.omniauth :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET"], callback_url: "http://localhost:3000/users/auth/facebook/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
