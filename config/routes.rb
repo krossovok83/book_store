b6bace07c3216e8bd335bf160bb2cf38804b7b64
krossovok83 <krossovok@ukr.net>
2022-02-02 13:58:57 +0200

    Add author page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users #, ActiveAdmin::Devise.config
+  devise_for :users # , ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :books
+  resources :books, only: %i[index show]
   root "home#home"
   resources :authors, except: [:show]
   resources :categories, except: [:show]
-  get "/generate_books", to: "books#generate"
 end
